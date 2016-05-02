c47803b48b4cfd589df41eb10833b1963b3a75ad
nahidalam <shapla@gmail.com>
2016-05-02 16:34:55 -0700

    updated changes for TI XBee comms
    UART3 is used for XBee

--- Xbee/Xbee.ino
+++ Xbee/Xbee.ino
@@ -1,29 +1,43 @@
+
+int incomingByte = 0;   // for incoming serial data
+#define LED RED_LED
+
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);   
+  Serial3.begin(9600);
+  //pinMode(13, OUTPUT); 
+  pinMode(LED, OUTPUT);    
 }
 
 void loop()
 {
-  //Serial.println('H');
+  //Serial3.println('H');
   //delay(1000);
-  Serial.println('L');
+  //Serial3.println('L');
+  Serial3.println("Sending command to the XBee");
   delay(1000);
-  //Serial.println('R1');
-  //delay(1000);
+  Serial3.println("I am R1");
+  delay(1000);
   
-  if (Serial.available() > 0) {
+  if (Serial3.available() > 0) {
                 // read the incoming byte:
-                //incomingByte = Serial.read();
-                //if incomingByte='A' blink
+                Serial3.println("got ack");
+                delay(1000);
+                incomingByte = Serial3.read();
+                Serial3.println("read done");
+                delay(1000);
 
-           
+                // say what you got:
+                //Serial.print("I received: ");
                 //Serial.println(incomingByte, DEC);
-                
-                digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-                delay(1000);              // wait for a second
-                digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-                delay(1000);              // wait for a second
+                Serial3.println("I got");
+                delay(1000);
+                Serial3.println(incomingByte);
+                delay(1000);
+                //digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+                //delay(1000);               // wait for a second
+                //digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+                //delay(1000);               // wait for a second
         }
+  
 }
